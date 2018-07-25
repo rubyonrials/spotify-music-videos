fb41b50f72c6cbfc89806ce041de5678af66ad9f
rubyonrials <matthew.rials+github2@gmail.com>
2018-07-25 15:44:31 -0700

    production BASE_API_URL

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,4 +1,5 @@
-export const BASE_API_URL = 'http://localhost:5000';
+// export const BASE_API_URL = 'http://localhost:5000';
+export const BASE_API_URL = '';
 
 export const emptyState = {
   spotify: {},
